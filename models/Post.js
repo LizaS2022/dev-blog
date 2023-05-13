bdad60075b0a26de2f27f69f795b5006378a29c7
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-13 11:37:03 -0400

    Created a databse using the models and sequelize

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+
+const {Model, DataTypes } = require("sequelize");
+// we can inform where to create the table
+const sequelize = require("../config/connection");
+
+class Post extends Model {};
+
+    Post.init({
+        // title.content,creator_username, created_on
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        title: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+
+        description: {
+            type:DataTypes.TEXT,
+            allowNull: false,
+        },
+
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'user',
+                key:'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    });
+
+module.exports = Post;
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express =require("express");
 const controllers = require("./controllers");
 // requiring the file
 const sequelize = require("./config/connection");
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 const app = express();
+const models = require("./models");
 
 // connective tussie between the api and the server
 app.use(controllers);
 
 // connect to the database and then run the server
-sequelize.sync({}).then(() => {
+sequelize.sync({force:false}).then(() => {
     app.listen(PORT, () => {
         console.log("on port " + PORT);
     });
