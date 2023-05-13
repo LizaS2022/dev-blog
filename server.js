7f6d6b09a52465bdc385f0867b53866ff475b7c7
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-13 10:12:21 -0400

    wrote a test get request, it works

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express =require("express");
-
+const routes = require("./routes");
 const PORT = 3002;
 const app = express();
 
+// connective tussie between the api and the server
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("on port " + PORT);
