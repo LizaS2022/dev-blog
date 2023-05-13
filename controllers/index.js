50e9a565e1f5e7a1ca4ee4926499b9f2452576fb
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-13 13:23:21 -0400

    added port process.env.PORT

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const users = require("./users");
+const pages = require("./pages");
+const { route } = require("./users");
+
+// everything that goes to the pages folder doesnt need anything in the front
+router.use('/',pages);
 
 // localhost:3002/users
 router.use("/users", users);
--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express =require("express");
 const controllers = require("./controllers");
 // requiring the file
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+// we need to deploy to the server so that it binds to the nearest available port number
 const PORT = process.env.PORT || 3002;
 const app = express();
 const models = require("./models");
+
+
+// to access the public folder content
+app.use(express.static("public"));
+
+// we need the next three lines to use handlebars
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 
 // this is needed to do post requests
