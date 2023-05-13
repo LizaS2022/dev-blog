d614d9a32ee770c7f769423bd7935a0dd07d6fcb
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-13 15:06:15 -0400

    made dashboard and retrieve data

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -1,9 +1,24 @@
 const router  = require("express").Router();
+const { User } = require("../../models");
 
+//localhost:3002
 router.get("/", (req,res) => {
     res.render("homepage");
 })
 
+//localhost:3002/dashboard
 
+router.get("/dashboard/:user_id", async (req,res) => {
+    const userData = await User.findByPk(req.params.user_id, {
+        attributes: [
+            "username",
+            "email",
+        ]
+    });
+    
+    const actualUserData = userData.get({plain: true });
+    console.log(actualUserData);
+    res.render("dashboard",actualUserData);
+})
 
 module.exports = router;
